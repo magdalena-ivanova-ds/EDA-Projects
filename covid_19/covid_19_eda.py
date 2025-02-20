fcefd3c323187158ff349a901ca0cee045f34f75
magdalena-ivanova-ds <magdalena.ivanova.ds@gmail.com>
2025-02-20 16:27:49 +0200

    Data cleaning and preprocessing

--- covid_19/covid_19_eda.py
+++ covid_19/covid_19_eda.py
@@ -3,11 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Explore the dataset
+#Data Cleaning & Preprocessing
 pd.set_option('display.max_columns', None)
 covid19_df = pd.read_csv("country_wise_latest.csv")
 #print(covid19_df.isna().sum())  #no NA values
 #print(covid19_df.info())
 #print(covid19_df.describe())
 covid19_df["Death Rate"] = (covid19_df["Deaths"] / covid19_df["Confirmed"] * 100).round(2)
+covid19_df["Active Case Rate"] = (covid19_df["Active"] / covid19_df["Confirmed"] * 100).round(2)
+covid19_df.drop(columns=["Deaths / 100 Recovered"], inplace=True)
 print(covid19_df.head())
+
+#Exploratory Data Analysis (EDA) & Visualizations
